1a781bb7c39db58f08197bdc13b6acf47d65e9df
nick54785478 <40881607+nick54785478@users.noreply.github.com>
2024-10-02 19:03:30 +0800

    調整 Service

--- src/main/java/com/example/demo/domain/booking/service/TicketBookingService.java
+++ src/main/java/com/example/demo/domain/booking/service/TicketBookingService.java
@@ -118,6 +118,9 @@ public class TicketBookingService extends BaseDomainService {
 
 	/**
 	 * 查詢個人訂票資訊
+	 * 
+	 * @param username 使用者名稱
+	 * @return BookingQueriedData
 	 */
 	@Transactional
 	public BookingQueriedData queryBooking(String username) {
@@ -126,18 +129,20 @@ public class TicketBookingService extends BaseDomainService {
 
 		// 座位資料
 		List<TrainSeat> trainSeatList = trainSeatRepository.findByBookUuidIn(bookingUuidList);
-		// 座位 Map <bookUuid, Entity>
+		// 座位 Map <book Uuid, Entity>
 		Map<String, List<TrainSeat>> seatMap = trainSeatList.stream()
 				.collect(Collectors.groupingBy(TrainSeat::getBookUuid));
 
-		// 透過火車座位查出對應火車的 Uuid
+		// 透過火車座位查出對應火車的 Uuid 清單
 		List<String> trainUuidList = trainSeatList.stream().map(TrainSeat::getTrainUuid).collect(Collectors.toList());
-		// 查出火車資訊
+		// 透過 火車 Uuid 清單查出火車資訊
 		List<Train> trainList = trainRepository.findByUuidIn(trainUuidList);
+		// 火車 Map<火車 Uuid, Train>
 		Map<String, Train> trainMap = trainList.stream().collect(Collectors.toMap(Train::getUuid, Function.identity()));
 
-		// 透過火車座位查出對應火車的 Uuid
+		// 透過火車座位查出對應車票的 Uuid 清單
 		List<String> ticketUuidList = trainSeatList.stream().map(TrainSeat::getTicketUuid).collect(Collectors.toList());
+		// 車票 Map<車票 Uuid, Train>
 		Map<String, Ticket> ticketMap = ticketReposiotry.findByTicketNoIn(ticketUuidList).stream()
 				.collect(Collectors.toMap(Ticket::getTicketNo, Function.identity()));
 
@@ -145,49 +150,65 @@ public class TicketBookingService extends BaseDomainService {
 		bookingQueriedData.setUsername(username);
 
 		List<TrainSeatBookedData> bookedDatas = new ArrayList<>();
+
 		bookingList.stream().forEach(book -> {
 			TrainSeatBookedData bookedData = new TrainSeatBookedData();
-			if (!Objects.isNull(ticketMap.get(book.getTicketUuid()))) {
-				var ticketData = ticketMap.get(book.getTicketUuid());
-				bookedData.setFrom(ticketData.getFromStop()); // 起站
-				bookedData.setTo(ticketData.getToStop()); // 迄站
-			}
-
-			if (!Objects.isNull(trainMap.get(book.getTrainUuid()))) {
-				var trainData = trainMap.get(book.getTrainUuid());
-				bookedData.setNumber(trainData.getNumber()); // 火車車次
-				bookedData.setKind(TrainKind.fromLabel(trainData.getKind().getLabel()).getLabel()); // 火車名稱
-
-				// Map <站名, 發車時間>
-				Map<String, LocalTime> stopMap = trainData.getStops().stream()
-						.collect(Collectors.toMap(TrainStop::getName, TrainStop::getTime));
-
-				bookedData.setStartTime(
-						!Objects.isNull(stopMap.get(bookedData.getFrom())) ? stopMap.get(bookedData.getFrom()) : null); // 發車時間
-				bookedData.setArriveTime(
-						!Objects.isNull(stopMap.get(bookedData.getTo())) ? stopMap.get(bookedData.getTo()) : null);
-				log.debug("起站:{}, 迄站:{}, 發車時間:{}, 抵達時間:{}", bookedData.getFrom(), bookedData.getTo(),
-						bookedData.getStartTime(), bookedData.getArriveTime());
-			}
-			
-
-			if (!Objects.isNull(seatMap.get(book.getUuid()))) {
-				var seatList = seatMap.get(book.getUuid());
-				Map<String, TrainSeat> collectMap = seatList.stream()
-						.collect(Collectors.toMap(TrainSeat::getBookUuid, Function.identity()));
-
-				if (!Objects.isNull(collectMap.get(book.getUuid()))) {
-					var trainSeat = collectMap.get(book.getUuid());
-					bookedData.setSeatNo(trainSeat.getSeatNo());
-					bookedData.setTakeDate(trainSeat.getTakeDate());
-					bookedData.setActiveFlag(trainSeat.getActiveFlag());
-					bookedData.setBooked(trainSeat.getBooked());
-				}
-			}
+			this.setTrainSeatBookedData(bookedData, book, ticketMap, trainMap, seatMap);
 			bookedDatas.add(bookedData);
 		});
 
 		bookingQueriedData.setBookedDatas(bookedDatas);
 		return bookingQueriedData;
 	}
+
+	/**
+	 * 設置 Train Seat Booking 資料
+	 * 
+	 * @param bookedData 被設置的 Booking 資料
+	 * @param book       Booking 領域資料
+	 * @param ticketMap  車票 Map<車票 Uuid, Train> 資料
+	 * @param trainMap   火車 Map<火車Uuid, Train> 資料
+	 * @param seatMap    座位 Map<bookUuid, Entity> 資料
+	 */
+	private void setTrainSeatBookedData(TrainSeatBookedData bookedData, TicketBooking book,
+			Map<String, Ticket> ticketMap, Map<String, Train> trainMap, Map<String, List<TrainSeat>> seatMap) {
+		if (!Objects.isNull(ticketMap.get(book.getTicketUuid()))) {
+			var ticketData = ticketMap.get(book.getTicketUuid());
+			bookedData.setFrom(ticketData.getFromStop()); // 起站
+			bookedData.setTo(ticketData.getToStop()); // 迄站
+		}
+
+		if (!Objects.isNull(trainMap.get(book.getTrainUuid()))) {
+			var trainData = trainMap.get(book.getTrainUuid());
+			bookedData.setNumber(trainData.getNumber()); // 火車車次
+			bookedData.setKind(TrainKind.fromLabel(trainData.getKind().getLabel()).getLabel()); // 火車名稱
+
+			// Map <站名, 發車時間>
+			Map<String, LocalTime> stopMap = trainData.getStops().stream()
+					.collect(Collectors.toMap(TrainStop::getName, TrainStop::getTime));
+			// 設置發車時間
+			bookedData.setStartTime(
+					!Objects.isNull(stopMap.get(bookedData.getFrom())) ? stopMap.get(bookedData.getFrom()) : null);
+			// 設置抵達時間
+			bookedData.setArriveTime(
+					!Objects.isNull(stopMap.get(bookedData.getTo())) ? stopMap.get(bookedData.getTo()) : null);
+			log.debug("起站:{}, 迄站:{}, 發車時間:{}, 抵達時間:{}", bookedData.getFrom(), bookedData.getTo(),
+					bookedData.getStartTime(), bookedData.getArriveTime());
+		}
+
+		if (!Objects.isNull(seatMap.get(book.getUuid()))) {
+			var seatList = seatMap.get(book.getUuid());
+			Map<String, TrainSeat> collectMap = seatList.stream()
+					.collect(Collectors.toMap(TrainSeat::getBookUuid, Function.identity()));
+
+			if (!Objects.isNull(collectMap.get(book.getUuid()))) {
+				var trainSeat = collectMap.get(book.getUuid());
+				bookedData.setSeatNo(trainSeat.getSeatNo());
+				bookedData.setTakeDate(trainSeat.getTakeDate());
+				bookedData.setActiveFlag(trainSeat.getActiveFlag());
+				bookedData.setBooked(trainSeat.getBooked());
+			}
+		}
+	}
+
 }
